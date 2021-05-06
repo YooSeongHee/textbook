8cf2d7bc231c9e7a2f71264ff1d7a0fe63023e06
Yukbi <41108503+YooSeongHee@users.noreply.github.com>
2021-05-06 16:57:57 +0900

    Update mi_graph.py

--- mi_graph.py
+++ mi_graph.py
@@ -17,7 +17,7 @@ word_frq_dict = collections.Counter()
 ngram_frq_dict= collections.Counter()
 N = 0
 
-for root, dirs, files in os.walk(r'C:\Users\유성희\Desktop\script\text'):
+for root, dirs, files in os.walk(r'..\script\text'):
 	for fn in files:
 		fp = os.path.join(root, fn)
 		with open(fp, 'r', encoding = 'utf8', errors='ignore') as f:
