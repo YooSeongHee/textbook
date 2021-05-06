a9ffcdcea1d1ab98f78e272357b40d49401194f8
Yukbi <41108503+YooSeongHee@users.noreply.github.com>
2021-05-06 21:23:52 +0900

    Add files via upload

---
+++ refer/sorted.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 00:08:08 2020
+
+@author: 유성희
+"""
+
+#str
+a = sorted("instruction")
+
+#list
+b = sorted([4,2,1,3,7])
+c = sorted([[2,1,5],[9,2,1],[11,2,3]])
+
+#set
+d = sorted({3,2,1})
+
+#tuple
+e = sorted((3,2,1))
+
+#dict
+f = sorted({3:1,2:3,1:4})
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
---
+++ refer/tuple.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 15:44:12 2020
+
+@author: yukbi
+"""
+
+t1 = (100, 200, 'apple', 'banana')
+t2 = (300, 400)
+
+print(t1)
+print(t1[0])
+print(t1[1:])
+print(t1+t2)
+
+
+
+# tup1 = (((100, 200), 1), ((300, 400), 1))
+# tup2 = ('바나나', '딸기')
+
+# print(tup1[0][0])
+# print(tup1,tup2)
+
+
+
+# for t1 in tup1:
+#  	print(t1[0][1])
