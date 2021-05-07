792556cf41c859c6296b24d1a930885f6ff836d9
Yukbi <41108503+YooSeongHee@users.noreply.github.com>
2021-05-07 17:11:34 +0900

    Update word.py

--- code/word.py
+++ code/word.py
@@ -11,7 +11,7 @@ import numpy as np
 import os, re
 import collections
 
-for root, dirs, files in os.walk(r'C:\Users\유성희\Desktop\script\text'):
+for root, dirs, files in os.walk(r'..\script\text'):
 	for fn in files:
 		fp = os.path.join(root, fn)
 		with open(fp, 'r', encoding = 'utf8', errors='ignore') as f:
